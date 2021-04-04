36538adb1c8156c7ba83a8aad9b9d748618d7391
linne <alinnen94@googlemail.com>
2021-04-04 16:41:32 +0100

    Model class started

--- agents.py
+++ agents.py
@@ -1,15 +1,63 @@
 from mesa import Agent
+import random
+import math
 
+
+# Firm class
 class Firm(Agent):
     def __init__(self, id, model, w, m, inv, inv_min, inv_max, d,
                  p, p_min, p_max, delta, Phi_min, Phi_max, phi_min,
                  phi_max, theta, Theta, mc, openPosition, toFire,
                  gamma, lambda_, num_months_with_openpositions,
-                 typeB, typeA, m_buffer, dailyProduces):
+                 typeB, typeA, m_buffer, dailyProduces, b):
+
         # initialise the parent class with required parameters
         super().__init__(id, model)
+
     # following eq(13-14) in main paper
     def produce(self):
         self.dailyProduces = self.lambda_ * self.getTypeB().size()
         self.inv += self.lambda_ * self.getTypeB().size()
+
+    # following eq(5) in main paper
+    def newWage(self):
+        self.w = self.w * (1) # + getRnd().getDblFromTo(-self.delta, self.delta)
+
+    # following eq(6) and (7) in main paper
+    def updateInvRange(self):
+        self.inv_max =  self.Phi_max * self.d
+        self.inv_min = self.Phi_min * self.d
+
+    # def updateDemandForLabour(self):
+    #     if(self.openPosition > 0):
+    #       self.num_months_with_openpositions = b + 1
+    #     else:
+    #           self.w * = 0.9 # the firm filled all open positions, therefore it reduces the wage
+    #     if(self.num_months_with_openpositions == self.gamma):
+    #        self.w * = 1.1 # the firm had openpositions for gamma months, therefore it reduces the wage by 10%
+
+    # following eq(8) and (9) in main paper
+    def updatePriceRange(self):
+        self.p_max = self.phi_max * self.mc
+        self.p_min = self.phi_min * self.mc
+
+    def increasePrice(self):
+        self.p = self.p # * (1 + Sim.getRnd().getDblFromTo(0, self.theta)
+        print("increase price: " + self.p)
+
+    def decreasePrice(self):
+        self.p = self.p # (1 - Sim.getRnd().getDblFromTo(0, self.theta)
+        print("decrease price: " + self.p)
+
+
+# Househoid class
+class Household(Agent):
+    def __init__(self, id, model, w, m, c, typeB, typeA, dividend,
+                 employed, alpha, P):
+
+        # initialise the parent class with required parameters
+        super.__init__(id, model)
+
+    def updateConsumption(self):
+        self.c = 1 # math.min(self.m / self.P) * Math.exp(self.alpha), self.m / self.P
 
--- model.py
+++ model.py
@@ -3,3 +3,49 @@ This model is built following Lengnick(2013) paper on
 Journal of Economic Behavior & Organization 86 (2013) 102– 120.
 """
 
+from agents import Firm, Household
+from mesa import Model
+from mesa.datacollection import DataCollector
+from mesa.time import RandomActivation
+import numpy as np
+
+# Data collector functions
+
+
+# Return number of agents
+def get_num_agents(model):
+    agents = [a for a in model.schedule.agents]
+    return len(agents)
+
+
+# Sum of all agent savings
+def get_total_savings(model):
+    agent_savings = [a for a in model.schedule.agents]
+
+
+# Model class
+class MacroModel(Model):
+
+    # Default parameters as of pg. 110
+    def __init__(
+        self,
+        seed = 1,
+        H = 1000,
+        F = 100,
+        num_typeA = 7,
+        delta = 0.019,
+        phi_max = 1,
+        phi_min = 0.25,
+        alpha = 0.9,
+        Psi_price = 0.25,
+        Psi_quant = 0.25,
+        xi = 0.01,
+        beta = 50,
+        pi = 0.1,
+        n = 7,
+        gamma = 24,
+        lambda_ = 3,
+        Theta = 0.75
+    ):
+        self.H = H
+        
